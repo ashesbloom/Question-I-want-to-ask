166b875719e1b12773b1c6dc16bdc192e3bfb2ad
ashesbloom <mayankpandeydk123@gmail.com>
2024-02-10 05:54:42 +0530

    added footer for creator info

--- support/user_data.js
+++ support/user_data.js
@@ -1,13 +1,13 @@
 function user_response_yes(){
     Email.send({
         SecureToken : "eeea90b8-2805-426c-8dd6-445f948f58da",
-        // To : 'doctorvirtucare@gmail.com',
-        // From : "doctorvirtucare@gmail.com",
-        // Subject : "User response has been submitted!",
-        // Body : "<h3> User selected <br><h2><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>YES..</u></b></h2></h3>"
+        To : 'doctorvirtucare@gmail.com',
+        From : "doctorvirtucare@gmail.com",
+        Subject : "User response has been submitted!",
+        Body : "<h3> User selected <br><h2><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>YES..</u></b></h2></h3>"
     }).then(
       message => {
-        alert(message);
+        alert('Your response has forwarded to the creator!');
         location.reload();
         
       }
@@ -16,13 +16,13 @@ function user_response_yes(){
 function user_response_no(){
   Email.send({
       SecureToken : "eeea90b8-2805-426c-8dd6-445f948f58da",
-      // To : 'doctorvirtucare@gmail.com',
-      // From : "doctorvirtucare@gmail.com",
-      // Subject : "User response has been submitted!",
-      // Body : "<h3> User selected <br><h2><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>NO</u></b></h2></h3>"
+      To : 'doctorvirtucare@gmail.com',
+      From : "doctorvirtucare@gmail.com",
+      Subject : "User response has been submitted!",
+      Body : "<h3> User selected <br><h2><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>NO</u></b></h2></h3>"
   }).then(
     message => {
-      alert(message)
+      alert('Your response has forwarded to the creator!');
       location.reload();
     }
   );
